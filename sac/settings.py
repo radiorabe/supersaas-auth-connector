0dcd9fe45d492f83f1374a6bfbc20d7e6a3e039c
Lucas Bickel <hairmare@rabe.ch>
2025-12-11 11:25:29 +0100

    feat: add /logout endpoint

--- sac/settings.py
+++ sac/settings.py
@@ -12,6 +12,10 @@ SSO_REALM = config("SSO_REALM", default="rabe")
 SSO_CLIENT_ID = config("SSO_CLIENT_ID", default="supersaas-auth-connector")
 
 ERROR_REDIRECT_URL = config("ERROR_REDIRECT_URL", default="https://www.rabe.ch")
+LOGOUT_REDIRECT_URL = config(
+    "LOGOUT_REDIRECT_URL",
+    default="https://sso.rabe.ch/auth/realms/rabe/protocol/openid-connect/logout?redirect_uri=https%3A%2F%2Fwww.rabe.ch",
+)
 
 SUPERSAAS_ACCOUNT_NAME = config("SUPERSAAS_ACCOUNT_NAME", default="RaBe")
 SUPERSAAS_API_TOKEN = config("SUPERSAAS_API_TOKEN", cast=Secret, default="")
